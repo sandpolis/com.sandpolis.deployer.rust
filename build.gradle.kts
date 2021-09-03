10b026d627874189e9c20cfb057e5f0897353d57
Tyler Cook <tcc@sandpolis.com>
2021-09-02 23:04:46 -0500

    Add initial build/publish workflows

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+//============================================================================//
+//                                                                            //
+//                         Copyright © 2015 Sandpolis                         //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPL    //
+//  as published by the Mozilla Foundation.                                   //
+//                                                                            //
+//============================================================================//
+
+plugins {
+	id("sandpolis-module")
+	id("sandpolis-soi")
+	id("sandpolis-publish")
+}
+
+val buildLinuxAmd64 by tasks.creating(Exec::class) {
+	workingDir(project.getProjectDir())
+	commandLine(listOf("cross", "build", "--release", "--target=x86_64-unknown-linux-gnu"))
+}
+tasks.findByName("build")?.dependsOn(buildLinuxAmd64)
+
+val buildLinuxAarch64 by tasks.creating(Exec::class) {
+	workingDir(project.getProjectDir())
+	commandLine(listOf("cross", "build", "--release", "--target=aarch64-unknown-linux-gnu"))
+}
+tasks.findByName("build")?.dependsOn(buildLinuxAarch64)
+
+val buildWindowsAmd64 by tasks.creating(Exec::class) {
+	workingDir(project.getProjectDir())
+	commandLine(listOf("cross", "build", "--release", "--target=x86_64-pc-windows-gnu"))
+}
+tasks.findByName("build")?.dependsOn(buildWindowsAmd64)
+
+publishing {
+	publications {
+		create<MavenPublication>("mavenLinuxAmd64") {
+			groupId = "com.sandpolis"
+			artifactId = "distagent-linux-amd64"
+			version = project.version.toString()
+
+			artifact(project.file("target/x86_64-unknown-linux-gnu/release/distagent"))
+		}
+		tasks.findByName("mavenLinuxAmd64")?.dependsOn(buildLinuxAmd64)
+
+		create<MavenPublication>("mavenLinuxAarch64") {
+			groupId = "com.sandpolis"
+			artifactId = "distagent-linux-aarch64"
+			version = project.version.toString()
+
+			artifact(project.file("target/aarch64-unknown-linux-gnu/release/distagent"))
+		}
+		tasks.findByName("mavenLinuxAarch64")?.dependsOn(buildLinuxAarch64)
+
+		create<MavenPublication>("mavenWindowsAmd64") {
+			groupId = "com.sandpolis"
+			artifactId = "distagent-windows-amd64"
+			version = project.version.toString()
+
+			artifact(project.file("target/x86_64-pc-windows-gnu/release/distagent"))
+		}
+		tasks.findByName("mavenWindowsAmd64")?.dependsOn(buildWindowsAmd64)
+	}
+}
+
+tasks.findByName("clean")?.doLast {
+	delete("target")
+}
